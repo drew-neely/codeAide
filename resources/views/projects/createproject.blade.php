87978d3cd24618a727a0308bc5dc342ff076a004
OptimizedEfficiency <tyler.y.xu@hotmail.com>
2016-09-17 18:55:16 -0500

    CreateProject

---
+++ resources/views/projects/createproject.blade.php
@@ -0,0 +1,46 @@
+@extends('layout')
+
+@section('content')
+
+<body>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+ <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<form>
+			<div class="form-group" align="center">
+				<h4>Title</h4>
+				<textarea name="title" class="form-control"></textarea>
+			</div>
+
+			<div class="form-group" align="center">
+				<h4>Description</h4>
+				<textarea name="title" class="form-control"></textarea>
+			</div>
+
+			<div class="form-group" align="center">
+				<h4>Winner's Prize</h4>
+				<textarea name="title" class="form-control"></textarea>
+			</div>
+			<div class="form-group" align="center">
+				<h4>Participation Prize</h4>
+				<textarea name="title" class="form-control"></textarea>
+			</div>
+
+			</form>
+			</div>
+			</div>
+		
+
+
+
+	</div>
+
+
+</head>
+</body>
+
+@stop
