2a2ecd71133d0c97842798bebbfd0dd7e0c6f12a
OptimizedEfficiency <tyler.y.xu@hotmail.com>
2016-09-17 15:38:48 -0500

    Attempt to make homepage pretty
    sort of success but not really

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
 	<div class="row">
 		<div class="col-md-6">
 			<h3>Projects</h3>
-			<div class="panel-group" id="panel-365011">
+			
 				<div class="panel panel-default">
 					<div class="panel-heading">
 						 <a class="panel-title" data-toggle="collapse" data-parent="#panel-365011" href="#panel-element-985096">Collapsible Group Item #1</a>
@@ -73,7 +73,7 @@
 						</div>
 					</div>
 				</div>
-			</div>
+			
 		</div>
 		<div class="col-md-6">
 			<h3>Solutions</h3>
