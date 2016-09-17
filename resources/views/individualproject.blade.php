ff9c8dc720c493961680d22085482d00e332c614
OptimizedEfficiency <tyler.y.xu@hotmail.com>
2016-09-17 17:01:37 -0500

    Fixed front page!

--- resources/views/individualproject.blade.php
+++ resources/views/individualproject.blade.php
@@ -47,20 +47,22 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-12">
-			<div class="panel-group" id="panel-234811">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						 <a class="panel-title" data-toggle="collapse" data-parent="#panel-234811" href="#panel-element-574330">Collapsible Group Item #1</a>
-					</div>
-					<div id="panel-element-574330" class="panel-collapse collapse in">
-						<div class="panel-body">
-							Anim pariatur cliche...
-						</div>
-					</div>
+		<div class="panel-group">
+				  <div class="panel panel-default">
+				  	<div class="panel-heading">
+				  		<h4 class="panel-title">
+				  			<a data-toggle="collapse" href="#collapse5">Collapsible list group</a>
+				  		</h4>
+				  	</div>
+				  	<div id="collapse5" class="panel-collapse collapse">
+				  		<ul class="list-group">
+				  			<li class="list-group-item">One</li>
+
+				  		</ul>
+				  		
+				  	</div>
+				  </div>
 				</div>
-			</div>
-		</div>
 	</div>
 </div>
   <script src="js/jquery.min.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,53 +13,77 @@
 	<div class="row">
 		<div class="col-md-6">
 			<h3>Projects</h3>
-			
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						 <a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-365011" href="#panel-element-985096">Collapsible Group Item #1</a>
-					</div>
-					<div id="panel-element-985096" class="panel-collapse collapse in">
-						<div class="panel-body">
-							Anim pariatur cliche...
-						</div>
-					</div>
+				<div class="panel-group">
+				  <div class="panel panel-default">
+				  	<div class="panel-heading">
+				  		<h4 class="panel-title">
+				  			<a data-toggle="collapse" href="#collapse1">Collapsible list group</a>
+				  		</h4>
+				  	</div>
+				  	<div id="collapse1" class="panel-collapse collapse">
+				  		<ul class="list-group">
+				  			<li class="list-group-item">One</li>
+
+				  		</ul>
+				  		
+				  	</div>
+				  </div>
 				</div>
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						 <a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-365011" href="#panel-element-171599">Collapsible Group Item #2</a>
-					</div>
-					<div id="panel-element-171599" class="panel-collapse collapse">
-						<div class="panel-body">
-							Anim pariatur cliche...
-						</div>
-					</div>
+				
+				<div class="panel-group">
+				  <div class="panel panel-default">
+				  	<div class="panel-heading">
+				  		<h4 class="panel-title">
+				  			<a data-toggle="collapse" href="#collapse2">Collapsible list group</a>
+				  		</h4>
+				  	</div>
+				  	<div id="collapse2" class="panel-collapse collapse">
+				  		<ul class="list-group">
+				  			<li class="list-group-item">One</li>
+
+				  		</ul>
+				  		
+				  	</div>
+				  </div>
 				</div>
-			
 		</div>
-	</div>
 		<div class="col-md-6">
 			<h3>Solutions</h3>
-			
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						 <a class="panel-title" data-toggle="collapse" data-parent="#panel-365012" href="#panel-element-985096">Collapsible Group Item #1</a>
-					</div>
-					<div id="panel-element-985096" class="panel-collapse collapse in">
-						<div class="panel-body">
-							Anim pariatur cliche...
-						</div>
-					</div>
+
+			<div class="panel-group">
+				  <div class="panel panel-default">
+				  	<div class="panel-heading">
+				  		<h4 class="panel-title">
+				  			<a data-toggle="collapse" href="#collapse3">Collapsible list group</a>
+				  		</h4>
+				  	</div>
+				  	<div id="collapse3" class="panel-collapse collapse">
+				  		<ul class="list-group">
+				  			<li class="list-group-item">One</li>
+
+				  		</ul>
+				  		
+				  	</div>
+				  </div>
 				</div>
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						 <a class="panel-title collapsed" data-toggle="collapse" data-parent="#panel-365011" href="#panel-element-171599">Collapsible Group Item #2</a>
-					</div>
-					<div id="panel-element-171599" class="panel-collapse collapse">
-						<div class="panel-body">
-							Anim pariatur cliche...
-						</div>
-					</div>
+
+				<div class="panel-group">
+				  <div class="panel panel-default">
+				  	<div class="panel-heading">
+				  		<h4 class="panel-title">
+				  			<a data-toggle="collapse" href="#collapse4">Collapsible list group</a>
+				  		</h4>
+				  	</div>
+				  	<div id="collapse4" class="panel-collapse collapse">
+				  		<ul class="list-group">
+				  			<li class="list-group-item">One</li>
+
+				  		</ul>
+				  		
+				  	</div>
+				  </div>
 				</div>
+
 		</div>
 	</div>
 
