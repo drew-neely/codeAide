62043c334522b4700c1be247ca338e481077a7b1
Drew Neely <gpsdrew@gmail.com>
2016-09-18 09:00:11 -0400

    final

---
+++ app/Http/Controllers/submissionsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Http\Requests;
+use App;
+use Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class submissionsController extends Controller
+{
+    public function index($projectId){
+    	$projects = DB::table('projects')->get();
+    	$project = NULL;
+    	foreach ($projects as $p) {
+    		if($p->id == $projectId) {
+    			$project = $p;
+    		}
+    	}
+    	return view('submissions.index', compact('project'));
+    }
+
+    public function submit(Request $request, $projectId) {
+        $submission = new App\Submission;
+        $submission->code = $request->all()['code'];
+        $submission->userId = Auth::id();
+        $submission->project_id = $projectId;
+        $submission->save();
+        return Redirect::to('indivproject/' . $projectId);
+    }
+}
