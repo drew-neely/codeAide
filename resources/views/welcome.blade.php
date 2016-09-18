18120d0434705dad3c3c2caf012a95487509385c
OptimizedEfficiency <tyler.y.xu@hotmail.com>
2016-09-18 08:12:58 -0500

    Fix front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
  <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-6">
+		<div class="col-md-12">
 			<h3>Projects</h3>
 			
 			<div class="panel-group">
@@ -27,26 +27,6 @@
 					</div>
 				</div>
 			@endforeach
-			</div>
-
-		</div>
-		<div class="col-md-6">
-			<h3>Solutions</h3>
-			
-			<div class="panel-group">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a data-toggle="collapse" href="#collapse2">Collapsible list group</a>
-						</h4>
-					</div>
-					<div id="collapse2" class="panel-collapse collapse">
-						<ul class="list-group">
-							<li class="list-group-item">One</li>
-						</ul>
-					
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
