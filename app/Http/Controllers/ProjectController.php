3c88ce417fcad5aefdd0f068601071082bcd5a9c
Drew Neely <gpsdrew@gmail.com>
2016-09-18 04:59:44 -0400

    Fixed STUPID Issue

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,13 +9,13 @@ use App\Http\Requests;
 class ProjectController extends Controller
 {
     public function index($projectId) {
-    	$projects = DB::table('projects')->get();
-    	$project = NULL;
-    	foreach ($projects as $p) {
-    		if($p->id == $projectId) {
-    			$project = $p;
-    		}
-    	}
+        $projects = DB::table('projects')->get();
+        $project = NULL;
+        foreach ($projects as $p) {
+            if($p->id == $projectId) {
+                $project = $p;
+            }
+        }
 
         if(is_null($project)) {
             return "Project not found";
@@ -29,9 +29,29 @@ class ProjectController extends Controller
             }
         }
 
-        
+        $submissions = DB::table('submissions')->get();
+        $problemSubmissions = array();
+        foreach ($submissions as $s) {
+            if($s->project_id == $project->id) {
+                array_push($problemSubmissions, $s);
+            }
+        }
 
-    	return view('projects.individualproject', compact('project'));
+        $solutionUsernameCodePairs = array();
+        foreach($problemSubmissions as $s) {
+            $owner = 'Submitter not found';
+            foreach($users as $u) {
+                if($u->id == $s->userId) {
+                    $owner = $u->name;
+                }
+            }
+            $pair = array('username' => $owner, 'code' => $s->code);
+            array_push($solutionUsernameCodePairs, $pair);
+        }
+
+        $data = array('project' => $project, 'user' => $user, 'solutionUsernameCodePairs' => $solutionUsernameCodePairs);
+
+        return view('projects.individualproject', compact('data'));
     }
 
     public function createProject() {
@@ -40,6 +60,15 @@ class ProjectController extends Controller
     }
 
     public function bulletin() {
-        
+        $projects = DB::table('projects')->get();
+        // $title = NULL;
+        // $description = NULL;
+
+        // foreach ($projects as $p) {
+        //     $title = $p->title;
+        //     $description = $p->description;
+        // }
+        // $bullet = array('title' => $title, 'description'=> $description);
+        return view('projects.bulletin', compact('projects'));
     }
 }
